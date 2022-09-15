0084d709d9a64a4603d5959ec1077efcfcf8ee77
gjosh0925 <87830240+gjosh0925@users.noreply.github.com>
2022-09-15 17:19:02 -0500

    Delete environment.php
    Removing environment.php file since it should not be on main code

--- includes/environment.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$config['servername'] = 'localhost';
-$config['username'] = 'root';
-$config['password'] = 'TrumpetMan2019!';
-$config['dbName'] = 'muda';
-
-$config['system_path'] = "C:\Users\Josh\PhpstormProjects\MUDA\\";
