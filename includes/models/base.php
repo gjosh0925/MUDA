68a5399eec5453b0ca0067bd0bf8e81d9b9077b7
unknown <gjosh0925@gmail.com>
2022-09-30 15:29:03 -0500

    MUDA-30 creating initial drafting process page and adding params to backend

--- includes/models/base.php
+++ includes/models/base.php
@@ -12,4 +12,8 @@ class base {
     public function MakePersistant($obj){
         return DB_Go('makepersistant', $obj);
     }
+
+    public function FindAllByParams($params = array(), $orderby='ID'){
+        return DB_Go('FindAllByParams', $this, $params, $orderby);
+    }
 }
