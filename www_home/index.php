1e3faa3e59f8612545c6df5dcb82d4f0171b1f7e
Joshua Calvert <joshua.calvert98@gmail.com>
2022-10-03 11:24:04 -0500

    Added MUDA logo colors to .css, can be used as variables

--- www_home/index.php
+++ www_home/index.php
@@ -4,45 +4,115 @@ global $pageUser;
 
 
 ?>
-<h1>Welcome<?php echo (isset($pageUser)) ? ' ' . $pageUser->getNickName() : '!'; ?></h1>
-<h3>MUDA Schedule</h3>
-<! This is for you josh>
-<table class="table">
-    <thead>
-        <tr>
-            <th scope="col">Teams</th>
-            <th scope="col">Date</th>
-            <th scope="col">Field</th>
-            <th scope="col">Score</th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr>
-            <td>Spartans<br>Cobras</td>
-            <td>9/25/2022<br>8:45pm</td>
-            <td>4</td>
-            <td>2-1</td>
-        </tr>
-        <tr>
-            <td>Speeders<br>Alphas</td>
-            <td>9/25/2022<br>8:45pm</td>
-            <td>1</td>
-            <td>4-5</td>
-        </tr>
-        <tr>
-            <td>Alphas<br>Spartans</td>
-            <td>9/30/2022<br>6:15pm</td>
-            <td>3</td>
-            <td>0-2</td>
-        </tr>
-    </tbody>
-</table>
+<!--
+<div class="a">
+    <img src="images/muda_logo_black_horizontal.png" width="40%" height="40%">
+</div>
+-->
+
+<div class="info-blurb">
+    <b><u>Season Info</u></b>
+    <p>
+        This season we are excited to be playing at Overton Park in Memphis!<br>
+        <b>Address:</b> 1914 Poplar Ave #202, Memphis, TN 38104
+    </p>
+</div>
+
+<div style="display:flex; justify-content: center;">
+    <div style="justify-content: center" class="tble1-spacing">
+        <div class="card">
+            <table class="table table-striped" >
+                <thead style="background-color: #00b2a9">
+                <tr>
+                    <th scope="col">Field</th>
+                    <th scope="col" colspan="4" style="text-align: center">Game 1 (6-6:55)</th>
+                    <th scope="col">Score</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <th scope="row" style="text-align: center">1</th>
+                    <td>Andrew (Blue)</td>
+                    <td>VS</td>
+                    <td>Justin (Purple)</td>
+                    <td colspan="2" style="text-align: right">6-5</td>
+                </tr>
+                <tr>
+                    <th scope="row" style="text-align: center">2</th>
+                    <td>Evan (Black)</td>
+                    <td>VS</td>
+                    <td>Eric (Pink)</td>
+                    <td colspan="2" style="text-align: right">6-9</td>
+                </tr>
+                <tr>
+                    <th scope="row" style="text-align: center">3</th>
+                    <td>Mary (Green)</td>
+                    <td>VS</td>
+                    <td>Walls (Orange)</td>
+                    <td colspan="2" style="text-align: right">2-6</td>
+                </tr>
+                <tr>
+                    <th scope="row" style="text-align: center">4</th>
+                    <td>Andre (White)</td>
+                    <td>VS</td>
+                    <td>Kathleen (Red)</td>
+                    <td colspan="2" style="text-align: right">5-9</td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    <div style="justify-content: center" class="tble2-spacing">
+        <div class="card">
+            <table class="table table-striped" >
+                <thead style="background-color: #00b2a9">
+                <tr>
+                    <th scope="col">Field</th>
+                    <th scope="col" colspan="4" style="text-align: center">Game 2 (7:05-8)</th>
+                    <th scope="col">Score</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <th scope="row" style="text-align: center">1</th>
+                    <td>Andrew (Blue)</td>
+                    <td>VS</td>
+                    <td>Evan (Black)</td>
+                    <td colspan="2" style="text-align: right">8-6</td>
+                </tr>
+                <tr>
+                    <th scope="row" style="text-align: center">2</th>
+                    <td>Andre (White)</td>
+                    <td>VS</td>
+                    <td>Eric (Pink)</td>
+                    <td colspan="2" style="text-align: right">9-7</td>
+                </tr>
+                <tr>
+                    <th scope="row" style="text-align: center">3</th>
+                    <td>Mary (Green)</td>
+                    <td>VS</td>
+                    <td>Justin (Purple)</td>
+                    <td colspan="2" style="text-align: right">6-10</td>
+                </tr>
+                <tr>
+                    <th scope="row" style="text-align: center">4</th>
+                    <td>Walls (Orange)</td>
+                    <td>VS</td>
+                    <td>Kathleen (Red)</td>
+                    <td colspan="2" style="text-align: right">7-8</td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+<!--
 <?php if (!isset($pageUser)) { ?>
     <button onclick="location.href = 'login.php';">Login</button>
 <?php } else { ?>
     <button onclick="location.href = 'logout.php';">Logout</button>
 <?php } ?>
-
+-->
 
 <?php
 include_once 'footer.php';
