e6543aa006ae077d9e87e0085b46ee8c9285125e
Isaiah Williams <isaiahawill@gmail.com>
2022-10-04 18:30:25 -0500

    Designed league team page with table but no current nav bar or footer

---
+++ www_home/teams.php
@@ -0,0 +1,59 @@
+<?php //include_once 'header.php'; ?>
+
+    <style>
+
+        h1 {text-align: center;}
+
+        code {
+            font-family: Consolas,"courier new";
+            color: crimson;
+            background-color: #f1f1f1;
+            padding: 2px;
+            font-size: 105%;
+        }
+
+        table, th, td {
+            border: 1px solid black;
+            border-radius: 10px;
+
+            padding-top: 10px;
+            padding-bottom: 20px;
+            padding-left: 30px;
+            padding-right: 40px;
+            border-spacing: 100px;
+
+            color: white;
+            font-size: 105%;
+        }
+
+        tr{
+            style: width:150%;
+            style: height:200px;
+        }
+
+        table.center {
+            margin-left: auto;
+            margin-right: auto;
+        }
+
+    </style>
+
+
+<h1>League's Teams</h1>
+
+<table class="center"; style= "width:100%;">
+    <tr style = "height:200px;">
+        <th style="background-color:Crimson;"><a href="">Team Red</a></th>
+        <th style="background-color:DodgerBlue"><a href="">Team Blue</a></th>
+        <th style="background-color:ForestGreen;"><a href="">Team Green</a></th>
+    </tr>
+    <tr style = "height:200px;">
+        <th style="background-color:DarkOrange;"><a href="">Team Orange</a></th>
+        <th style="background-color:Yellow;"><a href="">Team Yellow</a></th>
+        <th style="background-color:Purple;"><a href="">Team Purple</a></th>
+    </tr>
+</table>
+
+
+<?php //include_once 'footer.php'; ?>
+
