6cb3834e9891d0b57b2db6349d46fd44878eb023
unknown <gjosh0925@gmail.com>
2022-10-03 21:45:01 -0500

    adding more to new season file and changing nav bar to include logo

--- www_home/header.php
+++ www_home/header.php
@@ -13,6 +13,13 @@ if(session_start() !== null){
 
 ?>
 
+<style>
+    .loginlogout:hover{
+        cursor: pointer;
+        color: black;
+    }
+</style>
+
 <!DOCTYPE html>
 <html lang="English">
 <head>
@@ -29,23 +36,51 @@ if(session_start() !== null){
 
 
 <?php if ($_SERVER['REQUEST_URI'] !== '/MUDA/www_home/login.php') { ?>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand nav-link" href="index.php"> MUDA <span class="sr-only">(current)</span></a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div class="navbar-nav">
-            <a class="nav-item nav-link" href="draft.php">Draft</a>
-            <a class="nav-item nav-link" href="">Teams</a>
-            <a class="nav-item nav-link" href="">Another Webpage</a>
+<!--<nav class="navbar navbar-expand-lg navbar-light" style="background-color: #00b2a9; padding: 20px;">-->
+<!--    <a class="navbar-brand nav-link" href="index.php"> MUDA <span class="sr-only">(current)</span></a>-->
+<!--    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">-->
+<!--        <span class="navbar-toggler-icon"></span>-->
+<!--    </button>-->
+<!--    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">-->
+<!--        <div class="navbar-nav">-->
+<!--            <a class="nav-item nav-link" href="draft.php">Draft</a>-->
+<!--            <a class="nav-item nav-link" href="">Teams</a>-->
+<!--            <a class="nav-item nav-link" href="">Another Webpage</a>-->
+<!--            <div style="float:right;">-->
+<!--            --><?php //if (!isset($pageUser)) { ?>
+<!--                <a class="nav-item nav-link" href="login.php">Login</a>-->
+<!--            --><?php //} else { ?>
+<!--                <a class="nav-item nav-link" href="logout.php">Log out</a>-->
+<!--            --><?php //} ?>
+<!--            </div>-->
+<!--        </div>-->
+<!--    </div>-->
+<!--</nav>-->
+
+    <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #00b2a9;">
+<!--        <a class="navbar-brand" href="#">MUDA</a>-->
+        <img src="images/muda_logo_black_horizontal.png" width="18%" height="18%">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarText">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Teams</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="draft.php">Draft</a>
+                </li>
+            </ul>
             <?php if (!isset($pageUser)) { ?>
-                <a class="nav-item nav-link" href="login.php">Login</a>
+                <span class="navbar-text loginlogout" onclick="window.location='login.php'">Login</span>
             <?php } else { ?>
-                <a class="nav-item nav-link" href="logout.php">Log out</a>
+                <span class="navbar-text loginlogout" onclick="window.location='logout.php'">Logout</span>
             <?php } ?>
         </div>
-    </div>
-</nav>
+    </nav>
 <?php } ?>
 
--- www_home/new_season.php
+++ www_home/new_season.php
@@ -2,9 +2,11 @@
 
 include_once 'header.php';
 
+//getting all of the admins
 $params = null;
 $params['fld'] = 'UserRole';
 $params['val'] = 'admin';
+$params['val2'] = 'adminPlayer';
 $admins = new user();
 $admins = $admins->FindAllByParams($params);
 
@@ -48,62 +50,113 @@ $admins = $admins->FindAllByParams($params);
 
 <style>
     .step{
-        margin: 20px 40px 40px 40px;
+        margin: 20px 100px 40px 100px;
         border: 3px solid #0c2340;
         border-radius: 5px;
         padding: 30px;
     }
 
     h3{
-        margin: 50px 0px 0px 50px;
+        margin: 50px 0px 0px 110px;
+    }
+
+    .progressSteps{
+        position:relative;
+        left:35%;
+        padding-bottom: 80px;
+    }
+
+    .check{
+        position: relative;
+        top: 45px;
+        right: 45px;
     }
 </style>
 
-<h1>Create New Season</h1>
-
-<form id='seasonForm' action="new_season.php" method="post">
-
-    <h3>Step 1: Season Info</h3>
-    <div id="step1" class="step">
-        <label>Season Name</label>
-        <input class="form-control" type="text" name="season_name"><br>
-        <label>Start Date</label>
-        <input class="form-control" type="date" name="start_date"><br>
-        <label>End Date</label>
-        <input class="form-control" type="date" name="end_date"><br>
-        <label>Playoff Date</label>
-        <input class="form-control" type="date" name="playoff_date"><br>
-        <label>Season Info</label>
-        <input class="form-control" type="text" name="season_info"><br>
-    </div>
-
-    <h3>Step 2: Upload Players</h3>
-    <div id="step2" class="step">
-
-        <input type="file" id="fileUpload">
-        <input type="button" id="upload" value="Upload">
-        <hr />
-        <div id="dvCSV"></div>
-    </div>
-
-    <h3>Step 3: Choose League Coordinators playing this season</h3>
-    <div id="step3" class="step">
-        <div class="col-auto my-1">
-            <label class="mr-sm-2" for="inlineFormCustomSelect">Preference</label>
-            <select class="custom-select mr-sm-2" id="inlineFormCustomSelect">
-                <option selected></option>
-                <option value="1">Playing</option>
-                <option value="2">Not Playing</option>
-            </select>
+<!--<div style="display:flex;">-->
+    <div id="progressDiv" style="background: #0c2340; width: 20%; color: white; position:fixed; top: 0; height: 100%; z-index: -1;padding-top:50px;">
+        <div class="progressSteps" style="padding-top: 30px;">
+            <i class="fa-2x fa-regular fa-circle-check check" style="background:#00b2a9; border-radius: 100%;"></i><h1>Step 1</h1>
+        </div>
+        <div class="progressSteps">
+            <i class="fa-2x fa-regular fa-circle-check check" style="border-radius: 100%;"></i><h1>Step 2</h1>
+        </div>
+        <div class="progressSteps">
+            <i class="fa-2x fa-regular fa-circle-check check" style="border-radius: 100%;"></i><h1>Step 3</h1>
+        </div>
+        <div class="progressSteps">
+            <i class="fa-2x fa-regular fa-circle-check check" style="border-radius: 100%;"></i><h1>Step 4</h1>
         </div>
     </div>
 
-    <h3>Step 4: Choose Captains</h3>
-    <div id="step4" class="step">
-        Please complete Step 3 first
-    </div>
+    <form id='seasonForm' action="new_season.php" method="post" style="position: absolute; width:80%; left: 20%;">
+        <h1 style="text-align:center;">Create New Season</h1>
+        <h3>Step 1: Season Info</h3>
+        <div id="step1" class="step">
+            <label>Season Name</label>
+            <input class="form-control" type="text" name="season_name"><br>
+            <div style="display:flex; justify-content: space-between;">
+                <div style="display:flex; flex-direction: column">
+                    <label>Start Date</label>
+                    <input class="form-control" type="date" name="start_date"><br>
+                </div>
+                <div style="display:flex; flex-direction: column">
+                    <label>End Date</label>
+                    <input class="form-control" type="date" name="end_date"><br>
+                </div>
+                <div style="display:flex; flex-direction: column">
+                    <label>Playoff Date</label>
+                    <input class="form-control" type="date" name="playoff_date"><br>
+                </div>
+            </div>
+            <label>Season Info</label>
+            <textarea class="form-control" id=""></textarea>
+        </div>
 
-    <button type="submit" class="btn btn-secondary">Create Season</button>
-</form>
+        <h3>Step 2: Upload Players</h3>
+        <div id="step2" class="step">
+
+            <input type="file" id="fileUpload">
+            <input type="button" id="upload" value="Upload">
+            <hr />
+            <div id="dvCSV"></div>
+        </div>
+
+        <h3>Step 3: Choose League Coordinators playing this season</h3>
+        <div id="step3" class="step">
+            <table style="width:100%;">
+                <thead>
+                    <tr>
+                        <th>Nickname</th>
+                        <th>Not Playing</th>
+                        <th>Playing</th>
+                    </tr>
+                </thead>
+                <?php
+                $rows = '';
+                foreach($admins as $admin) {
+                    $rows .= '<tr>'
+                           . '<td>' . $admin->getNickname() . '</td>'
+                           . '<td><div class="form-check">'
+                           . '<input class="form-check-input position-static" type="checkbox" id="blankCheckbox" value="option1" aria-label="...">'
+                           . '</div></td>'
+                           . '<td><div class="form-check">'
+                           . '<input class="form-check-input position-static" type="checkbox" id="blankCheckbox" value="option1" aria-label="...">'
+                           . '</div></td>'
+                           . '</tr>';
+                }
+                echo $rows;
+                ?>
+            </table>
+        </div>
+
+        <h3>Step 4: Choose Captains</h3>
+        <div id="step4" class="step">
+            Please complete Step 3 first
+        </div>
+
+        <button type="submit" class="btn btn-secondary" style="    margin: 50px;position: relative;left: 25%;width: 40%;padding: 30px; font-size: 24px;">Create Season</button>
+    </form>
+<!--</div>-->
 
 <?php include_once 'footer.php'; ?>
