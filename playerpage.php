14321d0b34eb62017fc2141e25ee9a833ffa5f3a
unknown <dlagria73@gmail.com>
2022-10-17 21:02:49 -0500

    added the information to player page, and added checkboxes, but they don't really work after refresh, look more into that

--- playerpage.php
+++ playerpage.php
@@ -10,7 +10,9 @@ $teams = $teams->FindAllByParams($params);
 ?>
 <style>
 
-    h1 {text-align: center;}
+    h1 {
+        text-align: center;
+    }
 
     td, tr {
         border: 2px solid #0c2340;
@@ -36,36 +38,88 @@ $teams = $teams->FindAllByParams($params);
 <table id='playerTable' class="table sortable">
     <thead>
         <tr>
-            <th scope="col" style="width:14%">NickName</th>
-            <th scope="col" style="width:9%">Throwing</th>
-            <th scope="col" style="width:9%">Cutting</th>
-            <th scope="col" style="width:9%">Speed</th>
-            <th scope="col" style="width:10%">Conditioning</th>
-            <th scope="col" style="width:9%">Experience</th>
-            <th scope="col" style="width:14%">Buddy</th>
-            <th scope="col" style="width:9%">Height</th>
+            <th scope="col" style="width:14%">Name</th>
+            <th scope="col" style="width:9%">Phone Number</th>
+            <th scope="col" style="width:9%">Email</th>
             <th scope="col" style="width:9%">Gender</th>
-            <th scope="col" style="width:9%">Absence</th>
-            <th scope="col" style="width:8%">Playoffs</th>
+            <th scope="col" style="width:9%">Height</th>
+            <th scope="col" style="width:9%">Date of Birth</th>
+            <th scope="col" style="width:10%">Jersey</th>
+            <!-- Place a checkbox/button here -->
+            <th scope="col" style="width:9%">Have They Paid</th>
+            <!-- Place a checkbox/button here -->
+            <th scope="col" style="width:14%">Anticipated Days Missed</th>
+            <th scope="col" style="width:9%">Playoffs</th>
+            <th scope="col" style="width:9%">Buddy Request</th>
+            <th scope="col" style="width:9%">Willing to be Captain?</th>
+            <th scope="col" style="width:8%">Waiver Agreement</th>
+            <th scope="col" style="width:8%">Conditions/Illness/Injuries</th>
+            <th scope="col" style="width:8%">Experience</th>
+            <th scope="col" style="width:8%">Throwing</th>
+            <th scope="col" style="width:8%">Cutting</th>
+            <th scope="col" style="width:8%">Speed</th>
+            <th scope="col" style="width:8%">Conditioning</th>
         </tr>
     </thead>
     <tbody>
-        <td>
-            <th>
+        <tr>
+            <td>
                 <p>Josh</p>
-            </th>
-            <th>
-
-            </th>
-            <th>
-
-            </th>
-            <th>
-
-            </th>
-            <th>
-
-             </th>
-        </td>
+            </td>
+            <td>
+                <p>901-XXX-XXXX</p>
+            </td>
+            <td>
+                <p>XXXX@XXXXX.XXX</p>
+            </td>
+            <td>
+                <p>Helicopter</p>
+            </td>
+            <td>
+                <p>6"</p>
+            </td>
+            <td>
+                <p>02/29/2000</p>
+            </td>
+                <td>
+                    <input type="checkbox" id="jersey1" name="jersey1" value="YesJ">
+                </td>
+                <td>
+                    <input type="checkbox" id="paid1" name="paid1" value="YesP">
+                </td>
+            <td>
+                <p>3</p>
+            </td>
+            <td>
+                <input type="checkbox" id="answer1" name="answer1" value="YesY">
+            </td>
+            <td>
+                <p>Josh Glaser</p>
+            </td>
+            <td>
+                <input type="checkbox" id="answer1" name="answer1" value="YesY">
+            </td>
+            <td>
+                <p>Yes</p>
+            </td>
+            <td>
+                <p>Asthma</p>
+            </td>
+            <td>
+                <p>0</p>
+            </td>
+            <td>
+                <p>4</p>
+            </td>
+            <td>
+                <p>3</p>
+            </td>
+            <td>
+                <p>1</p>
+            </td>
+            <td>
+                <p>2</p>
+            </td>
+        </tr>
     </tbody>
 </table>
